f4f5d20d0599f9cb25cd67236c156179f6766889
ostibilas <lambgust@gmail.com>
2025-10-16 21:43:38 -0300

    não sei usar o git

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require ("express");
+const app = express();
+const PORT = 8080;
+
+   function helloWorld(req, res) {
+    res.send('<h1>Hello mundo!</h1>')
+}
+
+app.get("/hello", helloWorld);
+
+app.get('subtracao/:num1/:num2',(req,res)=>{
+
+    const num1StringSub = req.params.num1;
+    const num2StringSub = req.params.num2;
+    //puxa os numeros
+
+    const num1Sub = parseInt(num1StringSub);
+    const num2Sub = parseInt(num2StringSub);
+
+    if(( isNaN(num1Sub) || isNaN(num2Sub))){
+    return res.status(400).send('erro cade os numeros animal');
+    }
+
+    const resultado = num1Sub - num2Sub;
+    res.send(`O resultado da Subtração de ${num1} + ${num2} é: ${resultado}`)
+})
+
+// ===============================================================
+app.get('/soma/:num1/:num2',(req,res)=>{
+
+    const num1String = req.params.num1;
+    const num2String = req.params.num2;
+
+    const num1 = parseInt(num1String);
+    const num2 = parseInt(num2String);
+
+if(( isNaN(num1) || isNaN(num2))){
+    return res.status(400).send('erro cade os numeros animal');
+}
+
+const resultado = num1 + num2;
+res.send(`O resultado da soma de ${num1} + ${num2} é: ${resultado}`);
+})
+
+
+app.listen(PORT,()=>{
+
+console.log(`Servidor rodando na porta ${PORT}.`);
+
+
+})  
